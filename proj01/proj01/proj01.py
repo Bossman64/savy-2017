722b127aa5227fb9ee9d7a42304ce3d25dd9ba34
Bossman64 <zeke.cook2004@outlook.com>
2017-07-11 09:22:25 -0500

    Add files via upload

---
+++ proj01/proj01/proj01.py
@@ -0,0 +1,41 @@
+name = raw_input('Enter your name please:')
+Date = raw_input('What is today: ')
+Age = raw_input('How old are you:')
+Birthdaythisyear = raw_input('Have you had your birthday this year  Yes or No please:')
+print 'you will be 100 in the year'
+if Birthdaythisyear == 'yes':
+    Yes = 2017-int(Age)
+    x = Yes + 100
+    print x
+elif Birthdaythisyear == 'no':
+    No = int(Age) + 1
+    v = 2017 - No
+    y = v + 100
+    print y
+if int(Age) < 12:
+    print 'you may watch rated G movies or PG movies with an adults consent'
+elif 13<=int(Age)<18:
+    print 'you may watch PG,G, Or... PG-13 or R with parental guidance'
+elif int(Age) >= 18:
+    print'you may watch any movie without parental guidance'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if a user that is older than 100 uses my app then it will tell them the date that they turned 50
+
+#thsi is a test comment
+
+
