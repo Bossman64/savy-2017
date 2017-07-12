1d47d46ee7a65dbff6e6608a7d7716e79e84e9e3
Bossman64 <zeke.cook2004@outlook.com>
2017-07-12 15:22:37 -0500

    Add files via upload

--- proj04/proj04.py
+++ proj04/proj04.py
@@ -1,10 +1,28 @@
-# Name:
-# Date:
-
-"""
-proj04
-
-Asks the user for a string and prints out whether or not the string is a palindrome.
-
-"""
-
+# Name:zeke
+# Date:7/12/17
+#
+# """
+# proj04
+#
+# Asks the user for a string and prints out whether or not the string is a palindrome.
+#
+# """
+name = raw_input('Name: ')
+date = raw_input('Date: ')
+str = raw_input('What word would you like me to check to see if it is a palindrome:')
+lst = []
+for letter in str:
+    lst.append(letter)
+c = []
+for letter in str:
+    c.append(letter)
+a = lst
+print a
+#c = lst
+c.reverse()
+print lst, c
+#c = list
+if lst == c:
+    print'Congratulations you found a palindrome'
+if lst != c:
+    print'Sorry this is not a palindrome'
